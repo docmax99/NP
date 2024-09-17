e30e779804a86b1dfaad930cf0c52686d244fc21
docmax99 <149489762+docmax99@users.noreply.github.com>
2024-09-17 13:47:41 +0200

    Refactor layout and styling of text fields in boxing.js for improved alignment and spacing

--- src/pages/app/boxing.js
+++ src/pages/app/boxing.js
@@ -6,13 +6,13 @@ import TextField from '../components/TextField';
 
 export default function Boxing() {
     return( 
-        <div class="flex flex-col gap-4 bg-slate-400">
+        <div className="flex flex-col gap-4 bg-slate-400">
         
-        <div class="flex gap-4">
-            <div class="bg-red-400 w-1/2 h-24">
+        <div className="flex gap-4">
+            <div className="bg-red-400 w-1/2 h-24">
              <Image src="/Images/LogoNicePlaces.png" width={140} height={140} alt="Logo" className="mt-4 ml-6"/> 
              </div>
-            <div class="bg-yellow-300 w-1/2 h-24 flex flex-col "><Dropdown/></div> 
+            <div className="bg-yellow-300 w-1/2 h-24 flex flex-col "><Dropdown/></div> 
         </div>
 
     
@@ -28,26 +28,26 @@ export default function Boxing() {
         </div>
 
         
-        <div class="flex gap-4">
-            <div class="bg-red-400 w-1/3 h-64 mr-2 ml-4 rounded-3xl"><Image src="/Images/Hobbit.png" width={140} height={140} alt="Dreamhouse" className="w-full h-full object-cover  rounded-3xl "/></div>
-            <div class="bg-yellow-300 w-1/3 h-64 mr-2 ml-4 rounded-3xl"><Image src="/Images/Berghaus.png" width={140} height={140} alt="BergHaus" className="w-full h-full object-cover   rounded-3xl  "/></div>
-            <div class="bg-green-400 w-1/3 h-64 mr-4 ml-2 rounded-3xl"><Image src="/Images/Modernhouse.png" width={140} height={140} alt="Modernhouse" className="w-full h-full object-cover  rounded-3xl "/></div>
+        <div className="flex gap-4">
+            <div className="bg-red-400 w-1/3 h-64 mr-2 ml-4 rounded-3xl"><Image src="/Images/Hobbit.png" width={140} height={140} alt="Dreamhouse" className="w-full h-full object-cover  rounded-3xl "/></div>
+            <div className="bg-yellow-300 w-1/3 h-64 mr-2 ml-4 rounded-3xl"><Image src="/Images/Berghaus.png" width={140} height={140} alt="BergHaus" className="w-full h-full object-cover   rounded-3xl  "/></div>
+            <div className="bg-green-400 w-1/3 h-64 mr-4 ml-2 rounded-3xl"><Image src="/Images/Modernhouse.png" width={140} height={140} alt="Modernhouse" className="w-full h-full object-cover  rounded-3xl "/></div>
         </div>
 
         
-        <div class="bg-gray-500 w-full h-24"></div>
+        <div className="bg-gray-500 w-full h-24"></div>
 
         
-        <div class="flex gap-4">
-            <div class="bg-green-400 w-1/3 h-24"></div>
-            <div class="bg-red-400 w-1/3 h-24"></div>
-            <div class="bg-yellow-300 w-1/3 h-24"></div>
+        <div className="flex gap-4">
+            <div className="bg-green-400 w-1/3 h-24"></div>
+            <div className="bg-red-400 w-1/3 h-24"></div>
+            <div className="bg-yellow-300 w-1/3 h-24"></div>
         </div>
 
-        <div class="bg-gray-500 w-full h-24"></div>
+        <div className="bg-gray-500 w-full h-24"></div>
 
        
-        <div class="bg-green-400 w-full h-24"></div>
+        <div className="bg-green-400 w-full h-24"></div>
         
     </div>
           
