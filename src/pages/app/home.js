8a70b32a95a1997f0a8009e8e69649a276f3e725
docmax99 <149489762+docmax99@users.noreply.github.com>
2024-10-07 18:49:51 +0200

    Refactor houseHobbit.js to handle fallback values for description, image, and title

--- src/pages/app/home.js
+++ src/pages/app/home.js
@@ -68,25 +68,28 @@ const FeaturedListings = ({ houses, scrollContainerRef }) => (
         msOverflowStyle: 'none',
       }}
     >
-      {houses.map((house, index) => (
+      {houses.map((house) => (
         <div
-          key={index}
+          key={house.id}
           className="group relative overflow-hidden rounded-3xl shadow-lg transform transition duration-700 hover:scale-105 hover:shadow-2xl min-w-[300px] max-w-sm snap-start"
         >
-          <Link href={`/app/house/${house.id}`} passHref>
-            <Image
-              src={house.Bild1}
+          <Link href={`/app/houseHobbit?id=${house.id}`} legacyBehavior>
+            <a>
+            <img
+              src={house.Bild_3}
               width={400}
               height={300}
-              alt={house.title}
+              alt={house.Title}
               className="w-full h-64 object-cover rounded-3xl"
             />
+            
             <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-50"></div>
             <div className="absolute bottom-0 left-0 w-full p-4 text-white opacity-0 group-hover:opacity-100 transition duration-500">
-              <h2 className="text-xl font-bold">{house.title}</h2>
-              <p className="text-sm mt-1">{house.description}</p>
+              <h2 className="text-xl font-bold">{house.Title}</h2>
+              <p className="text-sm mt-1">{house.Beschreibung}</p>
             </div>
-          </Link>
+            </a>
+            </Link>
         </div>
       ))}
     </div>
