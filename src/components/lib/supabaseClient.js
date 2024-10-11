8d5f48bbabe9bb583464c36aabfc9fac123eaff8
Euppi <s232658@student.dhbw-mannheim.de>
2024-10-11 12:29:23 +0200

    WIR GEBEN UNSERE DATEN AN JEDEN

--- src/components/lib/supabaseClient.js
+++ src/components/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 // lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://evapvrkechtafxfxpkvn.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YXB2cmtlY2h0YWZ4Znhwa3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5Njc4NjksImV4cCI6MjA0MTU0Mzg2OX0.3hdMaOpgt5w5nuMJXsjGfgewzU-nDdcDCRS0cF1qVAk';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL und/oder Anon Key fehlen in den Umgebungsvariablen.');
