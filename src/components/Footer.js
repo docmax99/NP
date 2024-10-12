8c1818377e301fd8c9852cb81697416a0e131ad2
Euppi <s232658@student.dhbw-mannheim.de>
2024-10-12 19:38:48 +0200

    AUA, die ROSE HAT MICH GEBISSEN

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Footer = () => (
   <footer className="bg-white text-gray-800 p-8 mt-16 shadow-inner">
     <nav className="space-x-8 text-center text-lg">
-      <Link href="/about" passHref>
+      <Link href="/app/about" passHref>
         About Us
       </Link>
       <Link href="/contact" passHref>
